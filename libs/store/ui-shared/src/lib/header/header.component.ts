43454d3e3344122198ed6be26caddac386c172ce
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-06-07 11:51:06 +0300

    added code in header

--- libs/store/ui-shared/src/lib/header/header.component.ts
+++ libs/store/ui-shared/src/lib/header/header.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'lib-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() title = '';
+}
