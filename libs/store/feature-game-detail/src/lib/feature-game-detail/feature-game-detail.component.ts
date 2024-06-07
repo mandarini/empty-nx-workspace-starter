40cdac64033f1be098f0d76fad34dd9f2d78c626
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-06-07 12:21:32 +0300

    routes

--- libs/store/feature-game-detail/src/lib/feature-game-detail/feature-game-detail.component.ts
+++ libs/store/feature-game-detail/src/lib/feature-game-detail/feature-game-detail.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'lib-feature-game-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './feature-game-detail.component.html',
   styleUrl: './feature-game-detail.component.css',
 })
-export class FeatureGameDetailComponent {}
+export class FeatureGameDetailComponent {
+  constructor(private route: ActivatedRoute) {}
+
+  gameId$ = this.route.paramMap.pipe(
+    map((params: ParamMap) => params.get('id'))
+  );
+}
