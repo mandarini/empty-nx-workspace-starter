a023fd186b42e13cfd290afd5680b5f25d222aeb
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-06-07 10:55:28 +0300

    remove nx-welcome

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { getAllGames } from '../fake-api';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, MatCardModule, CommonModule],
+  imports: [RouterModule, MatCardModule, CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
