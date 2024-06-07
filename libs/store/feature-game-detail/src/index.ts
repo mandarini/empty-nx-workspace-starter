149ec2793a1ae8ab559b03dba54c0a15694fd022
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-06-07 12:15:34 +0300

    new route lib

---
+++ libs/store/feature-game-detail/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/feature-game-detail/feature-game-detail.component';
---
+++ libs/store/feature-game-detail/src/lib/feature-game-detail/feature-game-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-feature-game-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './feature-game-detail.component.html',
+  styleUrl: './feature-game-detail.component.css',
+})
+export class FeatureGameDetailComponent {}
