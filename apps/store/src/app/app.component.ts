bdaf07c5689a4a2f3079baf805f20bc0eda1b5b7
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-06-07 10:41:30 +0300

    added correct module impotrs

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { getAllGames } from '../fake-api';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, MatCardModule],
+  imports: [NxWelcomeComponent, RouterModule, MatCardModule, CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
